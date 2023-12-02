b4465a4e0fee7e3033d935c3052479e26ddfad64
Cody Sloan <csloan5@uwyo.edu>
2023-12-01 20:03:53 -0700

    Update aco_dispatch.py
    Added code to run ACO on a range of load demands

--- aco_dispatch.py
+++ aco_dispatch.py
@@ -3,12 +3,18 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
+import random
+import os
+
 
 # Path to csv file
 """
 Change path to where the generator_cost_emission_coefficients.csv file is located for your computer
 """
-path = "/home/fidelispyro/IntroAI/project/generator_cost_emission_coefficients.csv"
+# path = "/home/fidelispyro/IntroAI/project/generator_cost_emission_coefficients.csv"
+
+# This works for me (Cody), so hopefully it works for you guys too
+path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "generator_cost_emission_coefficients.csv")
 
 # Read csv file into a pandas dataframe
 df = pd.read_csv(path)
@@ -72,15 +78,50 @@ class ACO:
 
         return best_solution, best_cost
 
+def find_load_range(num, randomize = False):
+    """
+    Will return a list of load demands to use in the aco function. The values returned will be evely spread across the board by default,
+    but if the second parameter is set to true, then the values will be random. The number of values in the returned list will be 
+    whatever num is equal to.
+    """
+    if num <= 1:
+        return [283.5]
+    
+    load_demand_range = []
+
+    if randomize:
+        for _ in range(num):
+            load_demand_range.append(round(random.uniform(117,283.5), 2))
+    else:
+        inc_value = (283.5 - 117) / (num-1)
+        load_demand = 117
+        
+        for _ in range(num):
+            load_demand_range.append(load_demand)
+            load_demand += inc_value
+
+    return load_demand_range
+
 # Initialize ACO with parameters
 aco = ACO(num_ants = 40, num_iterations = 1000, alpha = 1, beta = 1, rho = 0.5, Q = 1)
-# Run ACO with set load demand
-best_solution, best_cost = aco.run(df, load_demand = 283.5)     # 283.5 MW is the total P (MW) of all 30 buses
+
+# This is how many times we will run aco on a different load demand
+number_of_load_demands = 3
+# Do you want random values? Or 5 evenly spread out values?
+randomize = True
+# Get range of load demands
+load_demands = find_load_range(number_of_load_demands, randomize)
+
+for ld in load_demands:
+    # Run ACO with set load demand
+    best_solution, best_cost = aco.run(df, load_demand = ld)     # 283.5 MW is the total P (MW) of all 30 buses
+    print(f"Running ACO using load demand of {ld}:")
+    print(f"Best solution: {best_solution}")
+    print(f"Best cost: {best_cost}")
+
 """ 
 Can still be optimized.
 Probably need to try to change load demand to a range of values to find the best solution for each load demand up to 283.5 MW.
 117 MW is the minimum output the generators can produce so load demand range should be from 117 MW to 283.5 MW.
 I want to run some matplotlib function to create some graphs we can use to show the current results and results of any updates we make.
-"""
-print(f"Best solution: {best_solution}")
-print(f"Best cost: {best_cost}")
+# """
